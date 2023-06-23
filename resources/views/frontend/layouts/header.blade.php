28937ae575455cc181a6cb2cbd7add621ee996bd
ranjeet <cnr.dev2@gmail.com>
2023-06-24 04:37:05 +0530

    first commit

---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!-- saved from url=(0048)https://mobirise.com/extensions/devm5/framework/ -->
+<html class="desktop mbr-site-loaded">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="generator" content="Mobirise v5.6.4, mobirise.com" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image:src" content="" />
+    <meta property="og:image" content="" />
+    <meta name="amethitech" content="DevM5 Theme Web Development Framework Demo"   />
+    <meta  name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+    <link  rel="shortcut icon"  href="https://mobirise.com/extensions/devm5/framework/assets/images/logo.png"  type="image/x-icon"   />
+    <meta  name="description"  content="New DevM5 Theme HTML Template - Download Now!" />
+    <title>Amethitech Web Development Company</title>
+    <link rel="stylesheet"  href="{{ asset('assets/css/mobirise2.css') }}" />
+    <link rel="stylesheet"  href="{{ asset('assets/css/bootstrap.min.css') }}"/>
+    <link rel="stylesheet"  href="{{ asset('assets/css/bootstrap-grid.min.css') }}"/>
+    <link rel="stylesheet"  href="{{ asset('assets/css/bootstrap-reboot.min.css') }}"/>
+    <link rel="stylesheet"  href="{{ asset('assets/css/style.css') }}"/>
+    <link rel="stylesheet"  href="{{ asset('assets/css/styles.css') }}"/>
+    <link rel="stylesheet"  href="{{ asset('assets/css/style(1).css') }}"/>
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    {{-- <link rel="stylesheet"  href="{{ asset('assets/css/find.css') }}"/> --}}
+    {{-- <link rel="stylesheet"  href="{{ asset('assets/css/changename.css') }}" as="style" onload="this.onload=null;this.rel=&#39;stylesheet&#39;" /> --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono&amp;display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap" /></noscript>
+    <link rel="preload"  as="style"  href="{{ asset('assets/css/mbr-additional.css') }}" />
+    <link rel="stylesheet"  href="{{ asset('assets/css/mbr-additional.css') }}" />  
+  </head>
