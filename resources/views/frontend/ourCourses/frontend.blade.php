aceb123cf8c573efcbd789920afd1919fb8be437
ranjeet <cnr.dev2@gmail.com>
2023-06-28 10:25:08 +0530

    ranjeetchanges

--- resources/views/frontend/ourCourses/frontend.blade.php
+++ resources/views/frontend/ourCourses/frontend.blade.php
@@ -1,6 +1,7 @@
 @extends('frontend.layouts.main')
 <script src="https://kit.fontawesome.com/26ae5e4e2d.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 @section('content')
  <section data-bs-version="5.1" class="features4 cid-tlVYmXxmF5" id="features05">
         <div class="container fs-4" id="services" style="font-family: 'Times New Roman', Times, serif">
@@ -191,4 +192,5 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 @endsection
